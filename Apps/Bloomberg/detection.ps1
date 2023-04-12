3c292c0b1dddd76015cb2ca0f501f45abd2e8f8e
Patrick Luijpers <patrick@luijpers.com>
2023-04-12 15:37:39 -0400

    Updated the detection script as version number was not correctly detected

--- Apps/Bloomberg/detection.ps1
+++ Apps/Bloomberg/detection.ps1
@@ -1,3 +1,27 @@
+[CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Path
+    )
+
+    try {
+        if ((Test-Path -Path $Path) -ne $true) {
+            return "0.0.0.0"
+        }
+    
+        $Major=$(Get-ChildItem $Path | %{$_.VersionInfo} | Select-Object *).FileVersionRaw.Major
+        $Minor= $(Get-ChildItem $Path | %{$_.VersionInfo} | Select-Object *).FileVersionRaw.Minor
+        $Build= $(Get-ChildItem $Path | %{$_.VersionInfo} | Select-Object *).FileVersionRaw.Build
+        $Revision= $(Get-ChildItem $Path | %{$_.VersionInfo} | Select-Object *).FileVersionRaw.Revision
+
+        return "$Major.$Minor.$Build.$Revision"
+    }
+    catch {
+        Write-Warning -Message "Unable to retrieve version information."
+        return "0.0.0.0"
+    }
+}
 
 $AppExpectedPath = "C:\blp\Wintrv\wintrv.exe"
 $AppName = "Bloomberg Anywhere Terminal"
@@ -5,20 +29,19 @@ $AppExpectedVersion = "3114.1.80.0"
 
 #------------------------------------------------------------------------------
 
-$AppInstalledVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($AppExpectedPath).FileVersion
-
 if ((Test-Path -Path $AppExpectedPath) -eq $false)
 {
     Write-Output "Installation of $AppName not found"
     exit 1 # Application not detected.
 }
 
+$AppInstalledVersion = GetAppVersion -Path $AppExpectedPath
 
-if ([System.Version]$AppInstalledVersion -ge [System.Version]$AppDetectionVersion) {
-    Write-Output "Success! The installed version of $AppName $AppInstalledVersion is equal or newer than expected version $AppInstalledVersion."
+if ([System.Version]$AppInstalledVersion -ge [System.Version]$AppExpectedVersion) {
+    Write-Output "Success! The installed version of $AppName $AppInstalledVersion is equal or newer than expected version $AppExpectedVersion."
     exit 0 # Application detected.
 }
 else {
-    Write-Output "Failure! The installed $AppName version $AppInstalledVersion is older than expected version $AppInstalledVersion."
+    Write-Output "Failure! The installed $AppName version $AppInstalledVersion is older than expected version $AppExpectedVersion."
     exit 1 # Application not detected.
 }
