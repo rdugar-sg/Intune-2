c22264a31a16cd793b1f1186f5144ca3152cbb86
Patrick Luijpers <Patrick@luijpers.com>
2023-04-24 08:51:38 -0400

    Update detection.ps1
    x32 and x64

--- Apps/Dell Command Update/detection.ps1
+++ Apps/Dell Command Update/detection.ps1
@@ -1,24 +1,37 @@
+# Run script as 32-bit process on 64-bit clients -> No
+# Enforce script signature check and run script silently -> No
 
-$AppExpectedPath = "${env:ProgramFiles(x86)}\Dell\CommandUpdate\dcu-cli.exe"
-$AppName = "Dell Command Update"
+
+$AppExpectedPath = "${env:ProgramFiles}\Dell\CommandUpdate\dcu-cli.exe"
+$AppExpectedPath2 = "${env:ProgramFiles(x86)}\Dell\CommandUpdate\dcu-cli.exe"
 $AppExpectedVersion = "4.8.0.0" 
 
 #------------------------------------------------------------------------------
 
-$AppInstalledVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($AppExpectedPath).FileVersion
 
-f ((Test-Path -Path $AppExpectedPath) -eq $false)
+if ((Test-Path -Path $AppExpectedPath) -eq $false)
 {
-    Write-Output "Installation of $AppName failed"
-    exit 1 # Application not detected.
+    if ((Test-Path -Path $AppExpectedPath2) -eq $false)
+    {
+        Write-Host "Installation Failed!"
+        exit 1 # Application not detected.
+    }
+    else
+    {
+        $AppInstalledVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($AppExpectedPath2).FileVersion
+    }
+}
+else
+{
+    $AppInstalledVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($AppExpectedPath).FileVersion
 }
 
 
 if ([System.Version]$AppInstalledVersion -ge [System.Version]$AppDetectionVersion) {
-    Write-Output "Success! The installed version of $AppName $AppInstalledVersion is equal or newer than expected version $AppInstalledVersion"
+    Write-Host "Success! Installed version $AppInstalledVersion is equal or newer than expected version $AppExpectedVersion."
     exit 0 # Application detected.
 }
 else {
-    Write-Output "Failure! The installed $AppName version $AppInstalledVersion is older than expected version $AppInstalledVersion"
+    Write-Host "Failure! Installed version $AppInstalledVersion is older than expected version $AppExpectedVersion."
     exit 1 # Application not detected.
 }
